f28eb46eb447452354d4cc9754cba99a66c29516
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-09-03 23:05:46 +0300

    Manually populating the data

---
+++ DataPopulation.sql
@@ -0,0 +1,10 @@
+INSERT INTO Restaurants
+VALUES ('Hungers', 'Surda, Ramallah', '02-2986197', '13:00');
+
+INSERT INTO MenuItems
+VALUES (4, 'Tasty Burger', '150 grams of meat, lettuce, tomato', 30);
+
+INSERT INTO Employees
+VALUES (4, 'Ahmad', 'Hamdan', 'Chef');
+
+SELECT * FROM Employees;
