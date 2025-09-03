4f279cfd97622d1994986ba17d3c3a984abd703b
Ahmad-K-Hamdan <Ahmadkhamdan@outlook.com>
2025-09-03 23:36:45 +0300

    Part: 3

--- Queries and Procedures.sql
+++ Queries and Procedures.sql
@@ -5,3 +5,9 @@ WHERE CustomerId = 231;
 -- Part: 2
 SELECT * FROM Employees
 WHERE Position = 'Manager';
+
+-- Part: 3
+SELECT * FROM Orders 
+JOIN OrderItems ON Orders.OrderId = OrderItems.OrderId
+JOIN MenuItems ON OrderItems.ItemId = MenuItems.ItemId 
+WHERE ReservationId = 280;
